4aa37869c9485e7edfce268dc2dac7fb58b8c0c9
santhosh2119 <59635756+santhosh2119@users.noreply.github.com>
2020-01-08 18:08:05 +0530

    Update ebils.php

--- Connections/ebils.php
+++ Connections/ebils.php
@@ -8,3 +8,6 @@ $username_ebils = "root";
 $password_ebils = "";
 $ebils = mysql_pconnect($hostname_ebils, $username_ebils, $password_ebils) or trigger_error(mysql_error(),E_USER_ERROR); 
 ?>
+<?php
+$connect = mysqli_connect("localhost", "root", "", "ebils");
+?>
