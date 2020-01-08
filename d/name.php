65797f7af918db935eff6763d0447abebaeb94ce
santhosh2119 <59635756+santhosh2119@users.noreply.github.com>
2020-01-08 18:07:57 +0530

    Update name.php

--- d/name.php
+++ d/name.php
@@ -2,20 +2,27 @@
 $connect = mysqli_connect("localhost", "root", "", "ebils");
 $number = count($_POST["name"]);
 $numbers = count($_POST["fname"]);
+$cname=$_POST['cname'];
 if($number > 1||$numbers > 1)
 {
 	for($i=0; $i<$number; $i++)
 		for($i=0; $i<$numbers; $i++)
 	{
-		if(trim($_POST["name"][$i] != ''||$_POST["fname"][$i] != ''))
+		if(trim($_POST["name"][$i] != '')||trim($_POST["fname"][$i] != ''))
 		{
 				$sql = "INSERT INTO tbl_name(name,fname) VALUES('".mysqli_real_escape_string($connect,$_POST["name"][$i])."','".mysqli_real_escape_string($connect,$_POST["fname"][$i])."')";
 			mysqli_query($connect, $sql);
 		}
 	}
+	$sql = "INSERT INTO pestcontrol(cname) VALUES('".mysqli_real_escape_string($connect,$_POST["cname"])."')";
+			mysqli_query($connect, $sql);
+	{
 	echo "Data Inserted";
+}
 }
 else
 {
 	echo "Please Enter Name";
 }
+
+?>
